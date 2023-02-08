0e59ef4eecc1a158bef4679a79971854cef8f9e5
nayoseb <nayoseb1018@gmail.com>
2023-02-08 16:05:56 +0900

    Initial commit

---
+++ src/main/java/com/codestates/todoappsoloproject/TodoappsoloprojectApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.todoappsoloproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappsoloprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappsoloprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/todoappsoloproject/TodoappsoloprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.todoappsoloproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappsoloprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
